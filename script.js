8c189846099917c6e986b9729bcd593d24c96ba5
Ahmad Fadli Karim <fadlykarim@users.noreply.github.com>
2025-11-22 13:19:19 +0700

    lh

--- script.js
+++ script.js
@@ -1716,8 +1716,8 @@ class ChickenCalc {
                 const col = c.keuntungan_bersih > 0 ? '#3F8F5F' : '#C8513A';
                 
                 // Fix: Use style="color:inherit" to ensure it takes the button's red color
-                const heartOutline = '<svg xmlns="http://www.w3.org/2000/svg" class="simple-icon" style="color:inherit" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path></svg>';
-                const heartFilled = '<svg xmlns="http://www.w3.org/2000/svg" class="simple-icon" style="color:inherit" viewBox="0 0 24 24" fill="currentColor" stroke="none"><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path></svg>';
+                const heartOutline = '<svg xmlns="http://www.w3.org/2000/svg" class="simple-icon" style="color:inherit; fill:none; stroke:currentColor" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path></svg>';
+                const heartFilled = '<svg xmlns="http://www.w3.org/2000/svg" class="simple-icon" style="color:inherit; fill:currentColor; stroke:currentColor" viewBox="0 0 24 24" fill="currentColor" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path></svg>';
                 const heart = c.is_favorite ? heartFilled : heartOutline;
                 
                 const modeBadge = c.is_advanced ? '<span class="history-tag">Advance</span>' : '';
@@ -1773,8 +1773,8 @@ class ChickenCalc {
                     console.error('Toggle favorite error:', error);
                     return;
                 }
-                const heartOutline = '<svg xmlns="http://www.w3.org/2000/svg" class="simple-icon" style="color:inherit" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path></svg>';
-                const heartFilled = '<svg xmlns="http://www.w3.org/2000/svg" class="simple-icon" style="color:inherit" viewBox="0 0 24 24" fill="currentColor" stroke="none"><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path></svg>';
+                const heartOutline = '<svg xmlns="http://www.w3.org/2000/svg" class="simple-icon" style="color:inherit; fill:none; stroke:currentColor" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path></svg>';
+                const heartFilled = '<svg xmlns="http://www.w3.org/2000/svg" class="simple-icon" style="color:inherit; fill:currentColor; stroke:currentColor" viewBox="0 0 24 24" fill="currentColor" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path></svg>';
                 target.innerHTML = newStatus ? heartFilled : heartOutline;
                 target.style.color = newStatus ? '#1e88e5' : '#567a60';
             });
